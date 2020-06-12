64c330cb9aa93da7c1fc385fcff36e3f46c3d3d1
JimmyHerrera <jimmy_hv1@hotmail.com>
2020-06-11 21:33:45 -0500

    Validación de formulario

--- src/app/agregar/agregar.component.ts
+++ src/app/agregar/agregar.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Usuario{
+  nombre:string;
+  correo:string;
+  password:string;
+}
 
 @Component({
   selector: 'app-agregar',
@@ -7,9 +14,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  formularioCreado: FormGroup
+  usuarios:Array<Usuario> = new Array<Usuario>();
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.crearFormulario();
+  }
+
+  crearFormulario(){
+    this.formularioCreado = this.formBuilder.group({
+      nombre: ['Carmen', Validators.required],
+      correo: ['', Validators.compose([
+        Validators.required,Validators.email
+      ])],
+      password: ['' , Validators.compose([
+        Validators.required, Validators.minLength(8)
+      ])]
+    })
+  }
+
+  agregar(){
+    this.usuarios.push(this.formularioCreado.value);
+    this.formularioCreado.reset();
   }
 
 }
