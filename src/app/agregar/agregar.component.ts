96d7f1c477a0c43b07587b9c3393e64aee6b6983
JimmyHerrera <jimmy_hv1@hotmail.com>
2020-06-12 13:05:50 -0500

    editar registros

--- src/app/agregar/agregar.component.ts
+++ src/app/agregar/agregar.component.ts
@@ -15,6 +15,8 @@ interface Usuario{
 export class AgregarComponent implements OnInit {
 
   formularioCreado: FormGroup
+  esNuevo:boolean = true
+  posicionEditar: number = -1;
   usuarios:Array<Usuario> = new Array<Usuario>();
 
   constructor(private formBuilder: FormBuilder) { }
@@ -40,4 +42,22 @@ export class AgregarComponent implements OnInit {
     this.formularioCreado.reset();
   }
 
+  editar(){
+    this.usuarios[this.posicionEditar].nombre =this.formularioCreado.value.nombre;
+    this.usuarios[this.posicionEditar].correo =this.formularioCreado.value.correo;
+    this.usuarios[this.posicionEditar].password =this.formularioCreado.value.password;
+    this.formularioCreado.reset();
+    this.esNuevo = true;
+    this.posicionEditar=-1;
+  }
+
+  editarUsuario(posicion:number){
+    this.formularioCreado.setValue({
+      nombre: this.usuarios[posicion].nombre,
+      correo: this.usuarios[posicion].correo,
+      password: this.usuarios[posicion].password
+    })
+    this.posicionEditar = posicion;
+    this.esNuevo = false;
+  }
 }
